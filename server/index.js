90aebfa632d3383e76cb89a97db9686d02afdef5
hrdk94 <goyalhardik220@gmail.com>
2025-07-17 04:46:55 +0530

    user model updated

--- server/index.js
+++ server/index.js
@@ -3,14 +3,23 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
+//connecting db
 dotenv.config();
 connectDB();
 
+//app essentials
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('GameRadar API running'));
-
+//listenign port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
+
+
+//app routes
+app.get('/', (req, res) => {
+    res.send('GameRadar API running')
+});
