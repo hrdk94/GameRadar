ee39bdcac8e3defd3865f079201e9da45ad2d190
hrdk94 <goyalhardik220@gmail.com>
2025-07-19 01:44:16 +0530

    about US route addded

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,6 +1,6 @@
 export default function AboutUs() {
   return (
-    <div style={{ padding: '20px' , alignItems:"left"}}>
+    <div style={{ padding: '20px' }}>
       <h1>Hi, This is created by @hrdk94</h1>
       <a href="https://github.com/hrdk94" target="_blank" rel="noopener noreferrer">GitHub</a><br />
       <a href="https://www.linkedin.com/in/hrdk94/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
